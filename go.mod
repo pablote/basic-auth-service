085af85d240679820ff9b00d2b9b4cd1dfa5dcef
Pablo Compagni <pablocompagni@gmail.com>
2021-11-15 14:33:12 -0300

    adds .env and mandatory env vars

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module basic-auth-service
 
 go 1.17
+
+require github.com/joho/godotenv v1.4.0 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"basic-auth-service/lib"
 	"fmt"
+	"github.com/joho/godotenv"
+	"log"
 	"net/http"
 	"os"
 )
@@ -10,19 +12,34 @@ import (
 const defaultPort = "10000"
 
 func main() {
+	// load .env file
+	err := godotenv.Load()
+	if err == nil {
+		fmt.Println("loaded config from .env file")
+	}
+
+	// load env
 	port, hasPort := os.LookupEnv("PORT")
 	if !hasPort {
 		port = defaultPort
 	}
 
-	username := os.Getenv("BASIC_AUTH_SERVICE_USERNAME")
-	password := os.Getenv("BASIC_AUTH_SERVICE_PASSWORD")
+	username, hasUsername := os.LookupEnv("BASIC_AUTH_SERVICE_USERNAME")
+	if !hasUsername {
+		log.Panic("BASIC_AUTH_SERVICE_USERNAME is mandatory")
+	}
 
+	password, hasPassword := os.LookupEnv("BASIC_AUTH_SERVICE_PASSWORD")
+	if !hasPassword {
+		log.Panic("BASIC_AUTH_SERVICE_PASSWORD is mandatory")
+	}
+
+	// http handler and listen
 	http.HandleFunc("/", lib.BasicAuthHandler(username, password))
 
 	addr := fmt.Sprintf("0.0.0.0:%s", port)
 	fmt.Printf("listening on %s\n", addr)
-	err := http.ListenAndServe(addr, nil)
+	err = http.ListenAndServe(addr, nil)
 	if err != nil {
 		fmt.Println(err)
 	}
