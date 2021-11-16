74b53beed917e0e34d579e9d912a478750303215
Pablo Compagni <pablocompagni@gmail.com>
2021-11-15 21:58:37 -0300

    adds unit test

---
+++ tests/BasicAuthHandler_test.go
@@ -0,0 +1,51 @@
+package tests
+
+import (
+	"basic-auth-service/lib"
+	"net/http/httptest"
+	"testing"
+)
+
+func Test_BasicAuthHandler(t *testing.T) {
+	table := []struct {
+		expectedUsername string
+		expectedPassword string
+		authorization string
+		assertCode int
+	}{
+		{
+			expectedUsername: "admin",
+			expectedPassword: "password",
+			authorization: "Basic YWRtaW46cGFzc3dvcmQ=", // admin:password
+			assertCode: 200,
+		},
+		{
+			expectedUsername: "admin",
+			expectedPassword: "admin",
+			authorization: "Basic YWRtaW46cGFzc3dvcmQ=", // admin:password
+			assertCode: 401,
+		},
+		{
+			expectedUsername: "admin",
+			expectedPassword: "password",
+			authorization: "",
+			assertCode: 401,
+		},
+	}
+
+	for _, row := range table {
+		target := lib.BasicAuthHandler(row.expectedUsername, row.expectedPassword, []string{"*"}, []string{"*"})
+
+		w := httptest.NewRecorder()
+		r := httptest.NewRequest("GET", "/", nil)
+		if len(row.authorization) > 0 {
+			r.Header.Set("Authorization", row.authorization)
+		}
+
+		target.ServeHTTP(w, r)
+
+		if w.Code != row.assertCode {
+			t.Errorf(`w.Code expected=%d actual=%d`, row.assertCode, w.Code)
+		}
+	}
+}
